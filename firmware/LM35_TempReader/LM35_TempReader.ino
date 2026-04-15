58902fde25d3a2fd9985f71a7dcc8edbcfe74bea
n23dcci073 <n23dcci073@student.ptithcm.edu.vn>
2026-04-15 23:08:30 +0700

    feat: doi dinh dang Serial sang JSON

--- firmware/LM35_TempReader/LM35_TempReader.ino
+++ firmware/LM35_TempReader/LM35_TempReader.ino
@@ -1,4 +1,5 @@
 int adcValues[2]; float nhietDo[2]; char chuoi[30];
+float temps[3];
 
 void loop() {
   adcValues[0] = analogRead(A0);
@@ -6,6 +7,15 @@ void loop() {
   adcValues[1] = analogRead(A1);
   nhietDo[1] = (adcValues[1] * 500.0) / 1023.0;
   sprintf(chuoi, "%d,%d\n", (int)nhietDo[0], (int)nhietDo[1]);
-  Serial.print(chuoi);
+  for (int i = 0; i < 3; i++) {
+    temps[i] = (adcValues[i] * 500.0) / 1023.0;
+  }
+  Serial.print("{\"temp1\": ");
+  Serial.print(temps[0]);
+  Serial.print(", \"temp2\": ");
+  Serial.print(temps[1]);
+  Serial.print(", \"temp3\": ");
+  Serial.print(temps[2]);
+  Serial.println("}");
   delay(100);
 }
